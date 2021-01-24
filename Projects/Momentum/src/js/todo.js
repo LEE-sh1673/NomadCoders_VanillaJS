e760014b262e3aaea63731190fa8bc8ec0732c44
LEE-sh1673 <lsh901673@gmail.com>
2021-01-24 22:46:07 +0900

    finish project. & bug fixed.

--- Projects/Momentum/src/js/todo.js
+++ Projects/Momentum/src/js/todo.js
@@ -1,94 +1,218 @@
-/*
-Local storage에는, 자바스크립트의 data를 저장할 수 없다.
-(오직 string 형식의 값만 가능)
+const PENDING = 0;
+const FINISHED = 1;
 
--> 즉, js는 local storage에 있는 모든 데이터를 string으로 저장한다.
-*/
-const toDoListContainer = document.querySelector(".js-toDoList-wrapper"),
-  toDoListWindow = toDoListContainer.querySelector(".js-toDoList-window"),
-  closeBtn = toDoListContainer.querySelector("#js-toDoList-header__close"),
-  toggleBtn = toDoListContainer.querySelector("button");
+// Objects for local storage and DOM manipulation.
+const taskObjs = [
+  {
+    id: PENDING, // equal to 0
+    name: "PENDING",
+    tasks: [],
+  },
+  {
+    id: FINISHED, // equal to 1
+    name: "FINISHED",
+    tasks: [],
+  },
+];
 
+Date.prototype.yyyymmdd = function () {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+
+  return [
+    this.getFullYear(),
+    (mm > 9 ? "" : "0") + mm,
+    (dd > 9 ? "" : "0") + dd,
+  ].join("");
+};
+
+// to-do list wrapper elements.
+const toDoFormWrapper = document.querySelector(".js-toDoList-wrapper"),
+  toDoListWindow = toDoFormWrapper.querySelector("div"),
+  toDoToggleBtn = toDoFormWrapper.querySelector("button");
+
+// to-do lists.
 const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+  toDoInput = toDoForm.querySelector("input");
+const pendingList = document.querySelector("[data-pending-list]");
+const finishedList = document.querySelector("[data-finished-list]");
 
-const TODOS_LS = "toDos";
+// count elements.
+const pendingTasks = document.querySelector("[data-pending-tasks]");
+const finishedTasks = document.querySelector("[data-finished-tasks]");
 
-let toDos = [];
+// btn toggle event.
+toDoToggleBtn.addEventListener("click", function () {
+  toDoListWindow.classList.toggle("showing--flex");
+});
 
-function DeleteToDo(event) {
+function UpdateTaskCount(id) {
+  if (id === PENDING) {
+    pendingTasks.innerText = taskObjs[PENDING].tasks.length;
+  } else {
+    finishedTasks.innerText = taskObjs[FINISHED].tasks.length;
+  }
+}
+
+function AppendTaskObj(taskList, taskObj) {
+  taskList.tasks.push(taskObj);
+}
+
+function CreateTaskObj(text) {
+  return {
+    id: String(Date.now()),
+    text,
+  };
+}
+
+function FilterTaskObj(taskList, taskObjId) {
+  taskList.tasks = taskList.tasks.filter(function (task) {
+    return task.id !== taskObjId;
+  });
+}
+
+function FindTaskObj(taskList, targetId) {
+  const taskObj = taskList.tasks.find(function (task) {
+    return task.id === targetId;
+  });
+  return taskObj;
+}
+
+function LoadFinishedList(taskList) {
+  const parsedTasks = JSON.parse(taskList);
+  parsedTasks.forEach(function (task) {
+    PaintFinishedTask(task);
+  });
+  UpdateTaskCount(FINISHED);
+}
+
+function LoadPendingList(taskList) {
+  const parsedTasks = JSON.parse(taskList);
+  parsedTasks.forEach(function (task) {
+    PaintPendingTask(task);
+  });
+  UpdateTaskCount(PENDING);
+}
+
+function SaveTaskList(taskList) {
+  localStorage.setItem(taskList.name, JSON.stringify(taskList.tasks));
+}
+
+function SwapTask(event) {
+  const currentList = Number(!this.checked);
+  const li = event.target.parentNode;
+  const targetTaskObj = FindTaskObj(taskObjs[currentList], li.id);
+
+  switch (currentList) {
+    case PENDING:
+      DeletePendingTask(event);
+      PaintFinishedTask(targetTaskObj);
+      break;
+    case FINISHED:
+      DeleteFinishedTask(event);
+      PaintPendingTask(targetTaskObj);
+      break;
+    default:
+      throw new Error("Unexpected task-list received.");
+  }
+}
+
+function DeletePendingTask(event) {
   const btn = event.target;
   const li = btn.parentNode;
-  toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function (toDo) {
-    return toDo.id !== parseInt(li.id);
+  pendingList.removeChild(li);
+  //   FilterTaskObj(taskObjs[PENDING], li.id);
+  taskObjs[PENDING].tasks = taskObjs[PENDING].tasks.filter(function (task) {
+    return task.id !== li.id;
   });
-  toDos = cleanToDos;
-  SaveToDos();
+  SaveTaskList(taskObjs[PENDING]);
+  UpdateTaskCount(PENDING);
 }
 
-function SaveToDos() {
-  /*
-	JSON.stringify(value)
-	JSON.stringify(value, replacer)
-	-> 자바스크립트의 object를 string으로 바꾸어준다.
-	(+)=> JSON(JavaScript Object Notation), 즉 데이터를 전달할 때, 자바스크립트가  그걸 다룰 수 있도록 object로 바꾸어 주는 기능(또는 obj->value)을 수행.*/
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function DeleteFinishedTask(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  finishedList.removeChild(li);
+  //   FilterTaskObj(taskObjs[FINISHED], li.id);
+  taskObjs[FINISHED].tasks = taskObjs[FINISHED].tasks.filter(function (task) {
+    return task.id !== li.id;
+  });
+  SaveTaskList(taskObjs[FINISHED]);
+  UpdateTaskCount(FINISHED);
 }
 
-function PaintToDo(text) {
+function CreateTaskElement(listId, text) {
   const li = document.createElement("li");
-  const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  const newId = toDos.length + 1;
+  const delBtn = document.createElement("button");
+  const checkBox = document.createElement("input");
 
-  delBtn.innerText = "❌";
-  delBtn.addEventListener("click", DeleteToDo);
   span.innerText = text;
-  li.appendChild(delBtn);
-  li.appendChild(span);
-  li.id = newId;
-  toDoList.appendChild(li);
+  delBtn.innerText = "Ｘ";
+  checkBox.type = "checkbox";
 
-  const toDoObj = {
-    text: text,
-    id: newId,
-  };
-  toDos.push(toDoObj);
-  SaveToDos();
+  if (listId == PENDING) {
+    delBtn.addEventListener("click", DeletePendingTask);
+    checkBox.checked = false;
+  } else {
+    delBtn.addEventListener("click", DeleteFinishedTask);
+    checkBox.checked = true;
+  }
+  checkBox.addEventListener("change", SwapTask);
+  li.append(checkBox, span, delBtn);
+  return li;
+}
+
+function PaintPendingTask(taskObj) {
+  const listItem = CreateTaskElement(PENDING, taskObj.text);
+  listItem.id = taskObj.id;
+  pendingList.appendChild(listItem);
+  taskObjs[PENDING].tasks.push(taskObj);
+  //   AppendTaskObj(taskObjs[PENDING], taskObj);
+  SaveTaskList(taskObjs[PENDING]);
+  UpdateTaskCount(PENDING);
+}
+
+function PaintFinishedTask(taskObj) {
+  const listItem = CreateTaskElement(FINISHED, taskObj.text);
+  listItem.id = taskObj.id;
+  finishedList.appendChild(listItem);
+  taskObjs[FINISHED].tasks.push(taskObj);
+  //   AppendTaskObj(taskObjs[FINISHED], taskObj);
+  SaveTaskList(taskObjs[FINISHED]);
+  UpdateTaskCount(FINISHED);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  const currentValue = toDoInput.value;
-  PaintToDo(currentValue);
+  const newTaskObj = CreateTaskObj(toDoInput.value);
+  PaintPendingTask(newTaskObj);
   toDoInput.value = "";
 }
 
-function LoadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
+function LoadTaskLists() {
+  taskObjs.forEach(function (taskObj) {
+    // get storage item by key.
+    const taskList = localStorage.getItem(taskObj.name);
 
-  if (loadedToDos !== null) {
-    // console.table([loadedToDos, JSON.parse(loadedToDos)]);
-    const parsedTodos = JSON.parse(loadedToDos);
-    parsedTodos.forEach(function (toDo) {
-      PaintToDo(toDo.text);
-    });
-  }
+    if (taskList !== null) {
+      switch (taskObj.id) {
+        case PENDING:
+          LoadPendingList(taskList);
+          break;
+        case FINISHED:
+          LoadFinishedList(taskList);
+          break;
+        default:
+          throw new Error("Unexpected taskList received.");
+      }
+    }
+  });
 }
 
 function init() {
-  LoadToDos();
+  LoadTaskLists();
   toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
-
-toggleBtn.addEventListener("click", function () {
-  toDoListWindow.style.display = "flex";
-});
-
-closeBtn.addEventListener("click", function () {
-  toDoListWindow.style.display = "none";
-});
