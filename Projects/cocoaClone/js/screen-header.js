60aa47386c96bca88df8ea002bdb2ce6d9191670
LEE-sh1673 <lsh901673@gmail.com>
2021-01-30 11:16:39 +0900

    Completed #6.34.

--- Projects/cocoaClone/js/screen-header.js
+++ Projects/cocoaClone/js/screen-header.js
@@ -1,4 +1,24 @@
 const screenHeader = document.querySelector(".screen-header");
+const chatScreen = document.getElementById("chat-screen");
+
+if (chatScreen !== null) {
+  const chatStatusBar = chatScreen.querySelector(".status-bar");
+  const chatHeader = chatScreen.querySelector(".alt-header");
+
+  if (chatHeader !== null) {
+    window.addEventListener("scroll", function () {
+      const currentHeight = window.scrollY;
+
+      if (currentHeight > 0) {
+        chatStatusBar.classList.add("blur");
+        chatHeader.classList.add("blur");
+      } else {
+        chatStatusBar.classList.remove("blur");
+        chatHeader.classList.remove("blur");
+      }
+    });
+  }
+}
 
 if (screenHeader !== null) {
   window.addEventListener("scroll", function () {
